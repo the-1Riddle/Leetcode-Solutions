c21630839b2dc64f24b928c843f310fc891b5621
Elvis Otieno <125451537+the1Riddle@users.noreply.github.com>
2024-03-02 00:37:10 +0300

    second-highest-salary.sql

--- MySQL/second-highest-salary.sql
+++ MySQL/second-highest-salary.sql
@@ -1,7 +1,17 @@
 # Time:  O(n)
 # Space: O(1)
 
+SELECT 
+AVG(salary) AS SecondHighestSalary FROM (SELECT id, salary,
+DENSE_RANK() OVER(ORDER BY salary DESC) AS ranking
+FROM Employee ) x WHERE x.ranking = 2;
+
+## /** OR **/
+
 SELECT (SELECT MAX(Salary) FROM Employee WHERE Salary NOT IN (SELECT MAX(Salary) FROM Employee)) SecondHighestSalary;
-# or
-SELECT (SELECT Salary FROM Employee GROUP BY Salary ORDER BY Salary DESC LIMIT 1,1) SecondHighestSalary;
+
+## /** OR **/
+
+SELECT 
+(SELECT DISTINCT(salary )FROM Employee ORDER BY salary DESC LIMIT 1,1) AS SecondHighestSalary;
 
