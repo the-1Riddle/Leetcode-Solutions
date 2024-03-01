2f09fe0da2d7389485a50b79280ac4630833ae8d
Elvis Otieno <125451537+the1Riddle@users.noreply.github.com>
2024-03-02 00:23:15 +0300

    combine-two-tables.sql

--- MySQL/combine-two-tables.sql
+++ MySQL/combine-two-tables.sql
@@ -1,6 +1,7 @@
 # Time:  O(n), n is size of Person Table
 # Space: O(n)
 
-SELECT FirstName, LastName, City, State FROM Person LEFT JOIN Address
-       ON Person.PersonId=Address.PersonId
+SELECT p.firstName, p.lastName, a.city, a.state 
+from Person p left outer join Address a 
+on p.personId=a.personId;
 
