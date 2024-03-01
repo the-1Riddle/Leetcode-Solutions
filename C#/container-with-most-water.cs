6b1ed0cd5202fa95179c4225bd3b5d6607af29cd
Elvis Otieno <125451537+the1Riddle@users.noreply.github.com>
2024-03-02 01:14:04 +0300

    container-with-most-water.cs

--- C#/container-with-most-water.cs
+++ C#/container-with-most-water.cs
@@ -1,1 +1,28 @@
 
+
+
+/** solution two **/
+
+public class Solution {
+    public int MaxArea(int[] height) {
+        int area = 0;
+        int j=0;
+        int k = height.Length-1;
+        List<int> myListArea = new List<int>();
+        while(j<k)
+        {
+            area = Math.Min(height[j], height[k]) * (k-j);
+            myListArea.Add(area);
+            if(height[j]<height[k])
+        {
+            j++;
+        }
+        else
+        {
+            k--;
+        }
+        }
+        
+        return myListArea.Max();
+    }
+}
