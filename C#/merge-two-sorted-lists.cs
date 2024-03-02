aebc5116f857beef8cf94440f91f0a1941c07fe7
Elvis Otieno <125451537+the1Riddle@users.noreply.github.com>
2024-03-02 23:33:30 +0300

    merge-two-sorted-lists.cs

---
+++ C#/merge-two-sorted-lists.cs
@@ -0,0 +1,68 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+    {
+        ListNode curr = new ListNode(0);
+        ListNode dummy = curr;
+
+        while (l1 != null && l2 != null)
+        {
+            if (l1.val < l2.val)
+            {
+                curr.next = l1;
+                l1 = l1.next;
+            }
+            else
+            {
+                curr.next = l2;
+                l2 = l2.next;
+            }
+            curr = curr.next;
+        }
+
+        curr.next = l1 ?? l2;
+
+        return dummy.next;
+    }
+}
+
+/** SOLUTION TWO **/
+
+public class Solution {
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+        
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+        
+        if (list1 != null) {
+            current.next = list1;
+        }
+        
+        if (list2 != null) {
+            current.next = list2;
+        }
+        
+        return dummy.next;
+    }
+}
