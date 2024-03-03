229031cea1e99eae46818d73038fc8a7b06e56db
Elvis Otieno <125451537+the1Riddle@users.noreply.github.com>
2024-03-03 11:42:51 +0300

    employees-earning-more-than-their-managers

--- MySQL/employees-earning-more-than-their-managers.sql
+++ MySQL/employees-earning-more-than-their-managers.sql
@@ -1,13 +1,13 @@
 # Time: O(n^2)
 # Space: O(1)
 
-SELECT e.Name AS Employee  FROM Employee e LEFT JOIN Employee b
-       ON e.ManagerId=b.Id
-       WHERE e.Salary > b.Salary
+SELECT e.Name AS Employee  FROM Employee e LEFT JOIN Employee b 
+        ON e.ManagerId=b.Id WHERE e.Salary > b.Salary
 
-SELECT Name AS Employee 
-    FROM Employee e 
-    WHERE e.ManagerId IS NOT NULL AND e.Salary > (SELECT Salary 
-                          FROM Employee 
-                          WHERE e.ManagerId = Id)
+/** # Write your MySQL query statement below **/
+       /** solution two **/
+
+SELECT e.name AS employee
+FROM employee AS e JOIN employee AS e1 ON e.managerid = e1.id
+WHERE e.salary > e1.salary;
 
