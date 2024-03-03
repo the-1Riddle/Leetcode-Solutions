6d90c23a04146287e0fe5e3d7ee57389f1671865
Elvis Otieno <125451537+the1Riddle@users.noreply.github.com>
2024-03-03 23:46:49 +0300

    rising-temperature

--- MySQL/rising-temperature.sql
+++ MySQL/rising-temperature.sql
@@ -1,8 +1,7 @@
 # Time:  O(n^2)
 # Space: O(n)
 
-SELECT wt1.Id 
-FROM Weather wt1, Weather wt2
-WHERE wt1.Temperature > wt2.Temperature AND 
-      TO_DAYS(wt1.DATE)-TO_DAYS(wt2.DATE)=1;
+SELECT t.id FROM Weather t
+JOIN Weather m ON t.recordDate  = DATE_ADD(m.recordDate, interval 1 day)
+WHERE t.temperature > m.temperature 
 
